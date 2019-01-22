ead3c0923cb2a915f4afd05f43045aba6324c37e
HuaYu <15600557119@163.com>
2019-01-22 22:42:44 +0800

    解决报错问题

---
+++ common/src/main/java/com.huayu.shiro.common.dto/ResultMessage.java
@@ -0,0 +1,27 @@
+package com.huayu.shiro.common.dto;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+/**
+ * Created by huayu on 2019/1/6.
+ */
+@ApiModel(value="ResultMessage",description="返回类型")
+@Data
+public class ResultMessage <T> {
+
+    @ApiModelProperty(value="状态信息",name="status")
+    private String status;
+
+    @ApiModelProperty(value="状态码",name="code")
+    private Integer code;
+
+    @ApiModelProperty(value="错误信息",name="msg")
+    private String msg;
+
+    @ApiModelProperty(value="相应消息",name="data")
+    private T data;
+
+
+}

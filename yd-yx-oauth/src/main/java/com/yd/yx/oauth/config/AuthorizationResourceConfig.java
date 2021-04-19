83673763eebf27c572d08ceda151061b58bb1261
shixh4 <shixh4@lenovo.com>
2021-04-19 16:49:01 +0800

    自定义用户名密码方式获取令牌

--- yd-yx-oauth/src/main/java/com/yd/yx/oauth/config/AuthorizationResourceConfig.java
+++ yd-yx-oauth/src/main/java/com/yd/yx/oauth/config/AuthorizationResourceConfig.java
@@ -1,12 +1,32 @@
 package com.yd.yx.oauth.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
 @Configuration
 @EnableResourceServer
 @Order(2)
-public class AuthorizationResourceConfig{
+public class AuthorizationResourceConfig extends ResourceServerConfigurerAdapter {
+    @Autowired
+    private OauthAuthenticationSucessHandler authenticationSucessHandler;
+    @Autowired
+    private OauthAuthenticationFailureHandler authenticationFailureHandler;
 
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.formLogin() // 表单登录
+                .loginProcessingUrl("/login") // 处理表单登录 URL
+                .successHandler(authenticationSucessHandler) // 处理登录成功
+                .failureHandler(authenticationFailureHandler) // 处理登录失败
+                .and()
+                .authorizeRequests() // 授权配置
+                .anyRequest()  // 所有请求
+                .authenticated() // 都需要认证
+                .and()
+                .csrf().disable();
+    }
 }
